e3a6140b310e34fb25112097c60cd04f7f687c0f
Tim Mein <mebusmein@gmail.com>
2016-03-24 17:47:26 +0100

    add connection object

--- src/alphareversi/Main.java
+++ src/alphareversi/Main.java
@@ -2,6 +2,12 @@ package alphareversi;
 
 public class Main {
     public static void main(String[] args) {
-        // AlphaReversi
+
+        Connection connection = Connection.getInstance();
+
+        connection.startConnection("127.0.0.1",1337);
+
+        connection.sendMessage("dit is een test string");
+
     }
 }
