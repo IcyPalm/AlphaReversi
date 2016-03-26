405f3e3b072bb1ef0d548abc57c56c5501acb7d2
Joost van Berkel <info@joostvanberkel.com>
2016-03-26 18:06:14 +0100

    Added error / ok command

---
+++ src/alphareversi/commands/receive/RecvStatusOkCommand.java
@@ -0,0 +1,15 @@
+package alphareversi.commands.receive;
+
+import alphareversi.commands.RecvCommand;
+
+/**
+ * Created by Joost van Berkel on 3/26/2016.
+ *
+ * S: OK
+ */
+public class RecvStatusOkCommand extends RecvCommand {
+    public RecvStatusOkCommand(String command)
+    {
+        this.setType(command);
+    }
+}
