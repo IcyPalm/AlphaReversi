e0da3713c9f2e08c43294ade611ce35abf799b79
Marijn Pool <info@marijnpool.nl>
2016-04-05 13:38:57 +0200

    Reverted comment

--- src/alphareversi/tests/TestParser.java
+++ src/alphareversi/tests/TestParser.java
@@ -9,20 +9,24 @@ import org.junit.Test;
 
 
 
-/**
- * Created by Joost van Berkel on 3/24/2016.
- *
- * <p>S: SVR GAMELIST ["--speltype--", ...] S: SVR PLAYERLIST ["--speler--", ...] S: SVR GAME MATCH
- * {GAMTYPE: "--speltype--", PLAYERTOMOVE: "--naam speler1--", OPPONENT: "--naam tegenstander--"} S:
- * SVR GAME YOURTURN {TURNMESSAGE: "--bericht voor deze beurt--"} S: SVR GAME MOVE {PLAYER:
- * "--speler--", DETAILS: "--reactie spel op zet--", MOVE: "--zet--"} S: SVR GAME --speler
- * resultaat-- {PLAYERONESCORE: "--score speler1--", PLAYERTWOSCORE: "--score speler2--", COMMENT:
- * "--commentaar op resultaat--"} S: SVR GAME --speler resultaat-- {PLAYERONESCORE: "--score
- * speler1--", PLAYERTWOSCORE: "--score speler2--", COMMENT: "Player forfeited match"} S: SVR GAME
- * --speler resultaat-- {PLAYERONESCORE: "--score speler1--", PLAYERTWOSCORE: "--score speler2--",
- * COMMENT: "Client disconnected"} S: SVR GAME CHALLENGE {CHALLENGER: "Sjors", GAMETYPE: "Guess
- * Game", CHALLENGENUMBER: "1"} S: SVR GAME CHALLENGE CANCELLED {CHALLENGENUMBER: "--uitdaging
- * nummer--"} S: SVR HELP --help informatie--
+ /**
+   * Created by Joost van Berkel on 3/24/2016.
+   *
+   * <p>S: SVR GAMELIST ["--speltype--", ...]
+   * S: SVR PLAYERLIST ["--speler--", ...]
+   * S: SVR GAME MATCH {GAMTYPE: "--speltype--", PLAYERTOMOVE: "--naam speler1--",
+   * OPPONENT: "--naam tegenstander--"}
+   * S: SVR GAME YOURTURN {TURNMESSAGE: "--bericht voor deze beurt--"}
+   * S: SVR GAME MOVE {PLAYER: "--speler--", DETAILS: "--reactie spel op zet--", MOVE: "--zet--"}
+   * S: SVR GAME --speler resultaat-- {PLAYERONESCORE: "--score speler1--",
+   * PLAYERTWOSCORE: "--score speler2--", COMMENT: "--commentaar op resultaat--"}
+   * S: SVR GAME --speler resultaat-- {PLAYERONESCORE: "--score speler1--",
+   * PLAYERTWOSCORE: "--score speler2--", COMMENT: "Player forfeited match"}
+   * S: SVR GAME --speler resultaat-- {PLAYERONESCORE: "--score speler1--",
+   * PLAYERTWOSCORE: "--score speler2--", COMMENT: "Client disconnected"}
+   * S: SVR GAME CHALLENGE {CHALLENGER: "Sjors", GAMETYPE: "Guess Game", CHALLENGENUMBER: "1"}
+   * S: SVR GAME CHALLENGE CANCELLED {CHALLENGENUMBER: "--uitdaging nummer--"}
+   * S: SVR HELP --help informatie--
  */
 public class TestParser {
 
