d6637089ffec717aef802128b732973a48147a44
Tim Mein <mebusmein@gmail.com>
2016-04-08 13:27:23 +0200

    Test for new Game

--- src/alphareversi/Main.java
+++ src/alphareversi/Main.java
@@ -1,85 +1,43 @@
 package alphareversi;
 
-import alphareversi.lobby.LobbyController;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-/**
- * Created by wouter on 16-3-2016.
- */
-
-public class Main extends Application {
-    static Scene baseScene;
-    static Stage primaryStage;
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("lobby/lobby.fxml"));
-        Parent root = loader.load();
-
-        this.primaryStage = primaryStage;
-        baseScene = new Scene(root);
-        primaryStage.setScene(baseScene);
-
-        LobbyController controller =
-                loader.<LobbyController>getController();
-        controller.setMainApp(this);
-        primaryStage.show();
-
-
-    }
-
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
-
-/*import alphareversi.game.tictactoemodule.TicTacToeModel;
+import alphareversi.commands.CommandListener;
+import alphareversi.commands.RecvCommand;
+import alphareversi.commands.receive.RecvGameMatchCommand;
+import alphareversi.game.tictactoemodule.TicTacToeModel;
 import alphareversi.game.tictactoemodule.TicTacToeViewController;
+import alphareversi.lobby.LobbyController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
-	
-	private Stage primaryStage;
+public class Main extends Application implements CommandListener {
+
+    private Stage primaryStage;
     private BorderPane rootLayout;
-    private TicTacToeModel ticTacToeModel;
-    
-    public Main(){
-    	ticTacToeModel = new TicTacToeModel();
+
+    public Main() {
     }
-    
+
     public static void main(String[] args) {
         // AlphaReversi
-    	launch();
+        launch();
     }
-    
+
     @Override
-	public void start(Stage primaryStage){
-        try{
-        	this.primaryStage = primaryStage;
+    public void start(Stage primaryStage) {
+        try {
+            this.primaryStage = primaryStage;
             this.primaryStage.setTitle("Tic Tac Toe");
-			initRootLayout();
-	        showTicTacToeView();
+            initRootLayout();
+            showLobby();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch(Exception e){
-        	e.printStackTrace();
-        }
-	}
-	
-	private void initRootLayout() throws Exception{
+    }
+
+    private void initRootLayout() throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("game/tictactoemodule/rootLayout.fxml"));
         rootLayout = (BorderPane) loader.load();
@@ -88,16 +46,40 @@ public class Main extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-	
-	private void showTicTacToeView() throws Exception{
+
+    private void showLobby() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("lobby/lobby.fxml"));
+        BorderPane view = (BorderPane) loader.load();
+
+        rootLayout.setCenter(view);
+
+        LobbyController controller = loader.<LobbyController>getController();
+        controller.setMainApp(this);
+    }
+
+    public void startGame(RecvGameMatchCommand command) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("game/tictactoemodule/ticTacToeView.fxml"));
         BorderPane ticTacToeView = (BorderPane) loader.load();
-        
+
         rootLayout.setCenter(ticTacToeView);
-        
+
+        TicTacToeModel ticTacToeModel = new TicTacToeModel();
+
         TicTacToeViewController controller = loader.getController();
         ticTacToeModel.setViewController(controller);
         controller.setTicTacToeModel(ticTacToeModel);
     }
-}*/
+
+    @Override
+    public void commandReceived(RecvCommand command) {
+        if (command instanceof RecvGameMatchCommand) {
+            try {
+                startGame((RecvGameMatchCommand) command);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
