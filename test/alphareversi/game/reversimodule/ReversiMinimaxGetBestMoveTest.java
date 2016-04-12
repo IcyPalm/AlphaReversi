1155bdd55051ce5286fa727c7017124269ade088
Maartenswag <maartenaj@hotmail.com>
2016-04-12 15:37:32 +0200

    Added third testcase with a board 5 moves towards an endstate

--- test/alphareversi/game/reversimodule/ReversiMinimaxGetBestMoveTest.java
+++ test/alphareversi/game/reversimodule/ReversiMinimaxGetBestMoveTest.java
@@ -14,7 +14,7 @@ public class ReversiMinimaxGetBestMoveTest extends TestCase {
     public ReversiMinimaxGetBestMoveTest() {
         model = new ReversiModel(1);
         // Create a test board
-        int[][] testBoard = testBoardTwo();
+        int[][] testBoard = testBoardThree();
         model.setBoard(testBoard);
         minimax = new ReversiMinimaxPlayer(model);
     }
@@ -27,7 +27,7 @@ public class ReversiMinimaxGetBestMoveTest extends TestCase {
         System.out.println("Waiting 5 seconds . . .");
 
         // Wait 5 seconds
-        Thread.sleep(2000);
+        Thread.sleep(2);
         System.out.println("Finished waiting 5 seconds . . .");
 
         // Fetch the best move from the minimax player.
@@ -70,6 +70,75 @@ public class ReversiMinimaxGetBestMoveTest extends TestCase {
         board[5][3] = 1;
         board[6][1] = 2;
         board[6][2] = 1;
+        return board;
+    }
+    
+    public int[][] testBoardThree() {
+        int[][] board = new int[8][8];
+        board[0][0] = 1;
+        board[0][1] = 2;
+        board[0][2] = 1;
+        board[0][3] = 2;
+        board[0][4] = 2;
+        board[0][5] = 1;
+        board[0][6] = 1;
+        board[0][7] = 0;
+        board[1][0] = 2;
+        board[1][1] = 1;
+        board[1][2] = 2;
+        board[1][3] = 1;
+        board[1][4] = 2;
+        board[1][5] = 1;
+        board[1][6] = 2;
+        board[1][7] = 0;
+        board[2][0] = 1;
+        board[2][1] = 2;
+        board[2][2] = 1;
+        board[2][3] = 2;
+        board[2][4] = 1;
+        board[2][5] = 1;
+        board[2][6] = 1;
+        board[2][7] = 0;
+        board[3][0] = 2;
+        board[3][1] = 1;
+        board[3][2] = 2;
+        board[3][3] = 1;
+        board[3][4] = 2;
+        board[3][5] = 2;
+        board[3][6] = 2;
+        board[3][7] = 1;
+        board[4][0] = 2;
+        board[4][1] = 2;
+        board[4][2] = 1;
+        board[4][3] = 2;
+        board[4][4] = 1;
+        board[4][5] = 2;
+        board[4][6] = 1;
+        board[4][7] = 2;
+        board[5][0] = 1;
+        board[5][1] = 2;
+        board[5][2] = 2;
+        board[5][3] = 1;
+        board[5][4] = 2;
+        board[5][5] = 1;
+        board[5][6] = 2;
+        board[5][7] = 1;
+        board[6][0] = 0;
+        board[6][1] = 1;
+        board[6][2] = 1;
+        board[6][3] = 2;
+        board[6][4] = 1;
+        board[6][5] = 2;
+        board[6][6] = 1;
+        board[6][7] = 2;
+        board[7][0] = 2;
+        board[7][1] = 0;
+        board[7][2] = 2;
+        board[7][3] = 1;
+        board[7][4] = 2;
+        board[7][5] = 1;
+        board[7][6] = 2;
+        board[7][7] = 1;
         return board;
     }
 
